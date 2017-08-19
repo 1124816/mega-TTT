f85847a73f14ac1c82a9e8c6e7e945f2b0adb3d7
Elias Basler <e.e.basler@gmail.com>
2017-08-18 23:45:59 -0400

    Fix macro function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(http);
 var path = require('path');
 var fs =require("fs");
 var players = 0;
+var turnEh = 1;
 var map = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
 
 app.use(express.static(path.join(__dirname, '/pub')));
@@ -15,21 +16,22 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
   players = players + 1;
-  socket.emit('matches', {map:map, players:players});
+  socket.emit('matches', {map:map, players:players, turn:turnEh});
 
   socket.on('play', function(msg){
   map[msg.x][msg.y] = msg.player;
   console.log(map);
-  socket.broadcast.emit('play', {x:msg.x, y:msg.y, player:msg.player});
+  turnEh = (3-msg.player);
+  socket.broadcast.emit('play', {x:msg.x, y:msg.y, player:msg.player, turn:turnEh});
   });
 
   socket.on('disconnect', function(){
   players = players - 1;
   console.log('user disconnected');
-  socket.emit('matches', {map:map, players:players});
+  socket.emit('matches', {map:map, players:players, turn:turnEh});
   });
 });
 
-http.listen(3000, function(){
+http.listen(4000, function(){
   console.log('listening on *:3000');
 });
